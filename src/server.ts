f96b20109e7e46fb4a86b84115503e8370f66c2b
fredrikkri <fjkri@hotmail.com>
2024-08-29 09:08:14 +0200

    good

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,15 @@
-// Importerer nødvendige moduler
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Project } from "./types";
 
-// Oppretter en ny Hono-applikasjon
 const app = new Hono();
 
-// Aktiverer CORS (Cross-Origin Resource Sharing) for alle ruter
 app.use("/*", cors());
 
-// Setter opp statisk filbetjening for filer i "static" mappen
 app.use("/static/*", serveStatic({ root: "./" }));
 
-// Initialiserer en liste med vaner (habits)
 const projects: Project[] = [
     // {
     //     "id": "1",
@@ -36,28 +31,22 @@ const projects: Project[] = [
     //   }
 ];
 
-// Definerer en POST-rute for å legge til nye vaner
 app.post("/add", async (c) => {
   const newProject = await c.req.json();
   console.log(newProject);
-  // Legger til den nye vanen i listen med en unik ID og tidsstempel
   projects.push({ id: crypto.randomUUID(), createdAt: new Date(), ...newProject });
 
-  // Returnerer den oppdaterte listen med vaner og en 201 (Created) statuskode
   return c.json(projects, { status: 201 });
 });
 
-// Definerer en GET-rute for å hente alle vaner
 app.get("/", (c) => {
   return c.json(projects);
 });
 
-// Definerer porten serveren skal lytte på
 const port = 3999;
 
 console.log(`Server is running on port ${port}`);
 
-// Starter serveren
 serve({
   fetch: app.fetch,
   port,
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 
-// Definerer et Zod-skjema for Habit
 export const ProjectSchema = z.object({
   id: z.string(),
   title: z.string(),
@@ -11,14 +10,10 @@ export const ProjectSchema = z.object({
   imgUrl: z.string()
 });
 
-// Definerer et Zod-skjema for å opprette en ny Habit
 export const ProjectCreateSchema = ProjectSchema.omit({ id: true });
 
-// Definerer et Zod-skjema for en array av Habit
 export const ProjectArraySchema = z.array(ProjectSchema);
 
-// Oppdatert type-definisjon basert på Zod-skjemaet
 export type Project = z.infer<typeof ProjectSchema>;
 
-// Oppdatert type-definisjon basert på Zod-skjemaet
 export type CreateProject = z.infer<typeof ProjectCreateSchema>;
