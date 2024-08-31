609c830b01814d75ae842871c4dffcddc8a1587a
fredrikkri <fjkri@hotmail.com>
2024-08-31 13:35:03 +0200

    endringer

--- src/server.ts
+++ src/server.ts
@@ -8,38 +8,37 @@ const app = new Hono();
 
 app.use("/*", cors());
 
-app.use("/static/*", serveStatic({ root: "./" }));
+app.use("/src/*", serveStatic({ root: "./" }));
 
 const projects: Project[] = [
-    // {
-    //     "id": "1",
-    //     "title": "test2",
-    //     "description": "Webapp",
-    //     "githubLink": "Link to github repo",
-    //     "liveDemoLink": "link",
-    //     "imgUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/330px-Unofficial_JavaScript_logo_2.svg.png",
-    //     "createdAt": new Date(),
-    //   },
-    //   {
-    //     "id": "2",
-    //     "title": "Website created with Python and Flask",
-    //     "description": "A simple website with sanity database",
-    //     "githubLink": "Link to github repo",
-    //     "liveDemoLink": "link",
-    //     "imgUrl": "https://blog.appseed.us/content/images/size/w600/2024/01/cover-flask.jpg",
-    //     "createdAt": new Date(),
-    //   }
+    {
+        "id": crypto.randomUUID(),
+        "title": "test2",
+        "description": "Webapp",
+        "githubLink": "Link to github repo",
+        "liveDemoLink": "link",
+        "imgUrl": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/330px-Unofficial_JavaScript_logo_2.svg.png",
+        "createdAt": new Date(),
+      },
+      {
+        "id": crypto.randomUUID(),
+        "title": "Website created with Python and Flask",
+        "description": "A simple website with sanity database",
+        "githubLink": "Link to github repo",
+        "liveDemoLink": "link",
+        "imgUrl": "https://blog.appseed.us/content/images/size/w600/2024/01/cover-flask.jpg",
+        "createdAt": new Date(),
+      }
 ];
 
 app.post("/add", async (c) => {
   const newProject = await c.req.json();
-  console.log(newProject);
   projects.push({ id: crypto.randomUUID(), createdAt: new Date(), ...newProject });
 
   return c.json(projects, { status: 201 });
 });
 
-app.get("/", (c) => {
+app.get("/", async (c) => {
   return c.json(projects);
 });
 
