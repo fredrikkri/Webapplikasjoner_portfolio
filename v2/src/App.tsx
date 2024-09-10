b8792e3c2a1030d1740a45b4dfd188afce06f380
fredrikkri <fjkri@hotmail.com>
2024-09-10 12:50:28 +0200

    begynt på oblig 2

---
+++ v2/src/App.tsx
@@ -0,0 +1,13 @@
+import Layout from "./components/Layout";
+import Title from "./components/Title";
+
+function App() {
+
+  return (
+    <Layout>
+      <Title title={"Portfolio"} />
+    </Layout>
+  );
+}
+
+export default App
